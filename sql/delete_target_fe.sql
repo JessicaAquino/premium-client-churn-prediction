c9aceb8b2a24f6a13844e493d1c54ca9c88d9cb6
JessicaAquino <jessica.aquino.torrez@gmail.com>
2025-12-03 21:19:14 -0300

    99% fe 1% probabilidades

---
+++ sql/delete_target_fe.sql
@@ -0,0 +1,49 @@
+ALTER TABLE df_init DROP COLUMN clase_binary_lag_1;
+ALTER TABLE df_init DROP COLUMN clase_binary_lag_2;
+ALTER TABLE df_init DROP COLUMN clase_binary_lag_3;
+ALTER TABLE df_init DROP COLUMN clase_binary2_lag_1;
+ALTER TABLE df_init DROP COLUMN clase_binary2_lag_2;
+ALTER TABLE df_init DROP COLUMN clase_binary2_lag_3;
+
+ALTER TABLE df_init DROP COLUMN clase_binary_delta_1;
+ALTER TABLE df_init DROP COLUMN clase_binary_delta_2;
+ALTER TABLE df_init DROP COLUMN clase_binary_delta_3;
+ALTER TABLE df_init DROP COLUMN clase_binary2_delta_1;
+ALTER TABLE df_init DROP COLUMN clase_binary2_delta_2;
+ALTER TABLE df_init DROP COLUMN clase_binary2_delta_3;
+
+ALTER TABLE df_init DROP COLUMN clase_binary_slope;
+ALTER TABLE df_init DROP COLUMN clase_binary2_slope;
+
+ALTER TABLE df_init DROP COLUMN clase_weight_lag_1;
+ALTER TABLE df_init DROP COLUMN clase_weight_lag_2;
+ALTER TABLE df_init DROP COLUMN clase_weight_lag_3;
+
+ALTER TABLE df_init DROP COLUMN clase_weight_delta_1;
+ALTER TABLE df_init DROP COLUMN clase_weight_delta_2;
+ALTER TABLE df_init DROP COLUMN clase_weight_delta_3;
+
+ALTER TABLE df_init DROP COLUMN clase_weight_slope;
+
+ALTER TABLE df_init DROP COLUMN clase_binary_max;
+ALTER TABLE df_init DROP COLUMN clase_binary_min;
+ALTER TABLE df_init DROP COLUMN clase_binary2_max;
+ALTER TABLE df_init DROP COLUMN clase_binary2_min;
+
+ALTER TABLE df_init DROP COLUMN clase_binary_lag_6;
+ALTER TABLE df_init DROP COLUMN clase_binary2_lag_6;
+ALTER TABLE df_init DROP COLUMN clase_binary_lag_12;
+ALTER TABLE df_init DROP COLUMN clase_binary2_lag_12;
+
+ALTER TABLE df_init DROP COLUMN clase_binary_delta_6;
+ALTER TABLE df_init DROP COLUMN clase_binary2_delta_6;
+ALTER TABLE df_init DROP COLUMN clase_binary_delta_12;
+ALTER TABLE df_init DROP COLUMN clase_binary2_delta_12;
+
+ALTER TABLE df_init DROP COLUMN clase_weight_max;
+ALTER TABLE df_init DROP COLUMN clase_weight_min;
+
+ALTER TABLE df_init DROP COLUMN clase_weight_lag_6;
+ALTER TABLE df_init DROP COLUMN clase_weight_lag_12;
+ALTER TABLE df_init DROP COLUMN clase_weight_delta_6;
+ALTER TABLE df_init DROP COLUMN clase_weight_delta_12;
